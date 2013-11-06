c96a19e0732cd4b271d06929ab0bec89bb245f94
Anton Domashnev <antondomashnev@gmail.com>
2013-11-06 23:29:21 +0400

    Initial Commit

---
+++ SocketIOBlocks/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SocketIOBlocks
+//
+//  Created by Антон Домашнев on 06.11.13.
+//  Copyright (c) 2013 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SocketIOBlocks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SocketIOBlocks
+//
+//  Created by Антон Домашнев on 06.11.13.
+//  Copyright (c) 2013 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
